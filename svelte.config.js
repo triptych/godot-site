2453d6a107af777743bab999e0006deb087d236e
Andrew Wooldridge <triptych@gmail.com>
2021-08-22 18:09:18 -0700

    adding netlify adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,16 @@
 /** @type {import('@sveltejs/kit').Config} */
+/** const sveltePreprocess = require('svelte-preprocess') **/
+import adapter from '@sveltejs/adapter-netlify';
+import preprocess from 'svelte-preprocess';
 const config = {
 	kit: {
+		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
-	}
+	},
+	preprocess: [
+		preprocess(),
+	]
 };
 
 export default config;
